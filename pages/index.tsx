37d3036f60b6170eae6b4fa824b30bc2d51903fc
raihan-778 <raihanarif778@gmail.com>
2023-02-23 17:39:21 +0600

    graphql query checked

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,14 @@ import Head from "next/head";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import NavBar from "@/components/NavBar/NavBar";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+  gql,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,4 +28,27 @@ export default function Home() {
       </main>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const httpLink = createHttpLink({
+    uri: "https://api.github.com/graphql",
+  });
+
+  const authLink = setContext((_, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+  return {
+    props: {},
+  };
 }
